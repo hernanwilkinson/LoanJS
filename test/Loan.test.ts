329b3a04a12428c2c9307ef2bb027e0f292c2fc6
Hernán Wilkinson <hernan.wilkinson@10pines.com>
2025-09-01 16:30:14 -0300

    Produces the amortization table

--- test/Loan.test.ts
+++ test/Loan.test.ts
@@ -1,6 +1,14 @@
 import { expect, test, it } from '@jest/globals';
 
 class FrenchLoan {
+    private constructor({capital, installmentsAmount, interestRate}: {
+        capital: number,
+        installmentsAmount: number,
+        interestRate: number
+    }) {
+
+    }
+
     static of({capital, installmentsAmount, interestRate}: {
         capital: number,
         installmentsAmount: number,
@@ -12,7 +20,13 @@ class FrenchLoan {
         if(installmentsAmount <= 0 || !Number.isInteger(installmentsAmount)) {
             throw new Error("Installments must be strictly positive integer")
         }
-        throw new Error("Interest rate must be strictly positive")
+        if(interestRate <= 0) {
+            throw new Error("Interest rate must be strictly positive")
+        }
+        return new FrenchLoan({capital, installmentsAmount, interestRate})
+    }
+    amortizationTable() {
+        return [{number: 1, initialBalance: 1, value: 101.0, interest: 1.0, amortization: 100.0, pendingBalance: 0}]
     }
 }
 
@@ -32,6 +46,9 @@ describe('Loan', () => {
     it('Cannot make a loan with a non integer installments amount', () => {
         expect(()=>FrenchLoan.of({capital: 1.0, installmentsAmount: 1.1, interestRate: 10})).toThrow("Installments must be strictly positive integer");
     });
-
+    it('Produces the amortization table ', () => {
+        const loan = FrenchLoan.of({capital: 100.0, installmentsAmount: 1, interestRate: 12})
+        expect(loan.amortizationTable()).toStrictEqual([{number: 1, initialBalance: 1, value: 101.0, interest: 1.0, amortization: 100.0, pendingBalance: 0}]);
+    });
     
 });
