5906d28ffd4c19e984fcd0323787e91c046839ef
Hernán Wilkinson <hernan.wilkinson@10pines.com>
2025-09-01 16:04:55 -0300

    Cannot make a loan with a non integer installments amount

--- test/Loan.test.ts
+++ test/Loan.test.ts
@@ -1,12 +1,16 @@
 import { expect, test, it } from '@jest/globals';
 
 class Loan {
-    static of(capital: number, installments: number, interestRate: number) {
+    static of({capital, installmentsAmount, interestRate}: {
+        capital: number,
+        installmentsAmount: number,
+        interestRate: number
+    }) {
         if(capital <= 0.0) {
             throw new Error("Capital must be strictly positive")
         }
-        if(installments <= 0.0) {
-            throw new Error("Installments must be strictly positive")
+        if(installmentsAmount <= 0 || !Number.isInteger(installmentsAmount)) {
+            throw new Error("Installments must be strictly positive integer")
         }
         throw new Error("Interest rate must be strictly positive")
     }
@@ -14,15 +18,20 @@ class Loan {
 
 describe('Loan', () => {
     test('Cannot make a loan with cero capital', () => {
-        expect(()=>Loan.of(0.0, 1, 10)).toThrow("Capital must be strictly positive");
+        expect(()=>Loan.of({capital: 0.0, installmentsAmount: 1, interestRate: 10})).toThrow("Capital must be strictly positive");
     });
 
     it('Cannot make a loan with cero installments', () => {
-        expect(()=>Loan.of(1.0,0, 10)).toThrow("Installments must be strictly positive");
+        expect(()=>Loan.of({capital: 1.0, installmentsAmount: 0, interestRate: 10})).toThrow("Installments must be strictly positive integer");
     });
 
     it('Cannot make a loan with cero interest rate', () => {
-        expect(()=>Loan.of(1.0,1, 0)).toThrow("Interest rate must be strictly positive");
+        expect(()=>Loan.of({capital: 1.0, installmentsAmount: 1, interestRate: 0})).toThrow("Interest rate must be strictly positive");
     });
- 
+
+    it('Cannot make a loan with a non integer installments amount', () => {
+        expect(()=>Loan.of({capital: 1.0, installmentsAmount: 1.1, interestRate: 10})).toThrow("Installments must be strictly positive integer");
+    });
+
+
 });
